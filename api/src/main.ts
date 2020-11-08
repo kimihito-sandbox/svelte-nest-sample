2e097de5ae4ddac0fae8f3c43aea60a1b58d493a
kimihito <tatsurotamashiro@gmail.com>
2020-11-08 16:57:23 +0900

    Replace fastify

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,14 +1,9 @@
-declare const module: any;
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({logger: true}));
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
